2875b2fe02f2637f7301dafe5ff7506222f02a35
Ben Tideswell <ben@fishpig.co.uk>
2017-01-09 13:37:23 +0000

    1.0.0.16

--- Block/AbstractBlock.php
+++ Block/AbstractBlock.php
@@ -32,15 +32,4 @@ abstract class AbstractBlock extends \Magento\Framework\View\Element\Template
 	    
         parent::__construct($context, $data);
     }
-	
-	public function toHtml()
-	{
-		try {
-			return parent::toHtml();
-		}
-		catch (\Exception $e) {
-			echo sprintf('<h1>Exception in %s</h1><p>%s</p><pre>%s</pre>', get_class($this), $e->getMessage(), $e->getTraceAsString());
-			exit;
-		}
-	}
 }
