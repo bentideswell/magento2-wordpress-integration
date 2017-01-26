95843bbe59c4b5f0cbbce9c45ad2a742d56f6d56
Ben Tideswell <ben@fishpig.co.uk>
2017-01-26 15:17:12 +0000

    1.0.0.30

--- Observer/InjectContent.php
+++ Observer/InjectContent.php
@@ -11,16 +11,18 @@ use \FishPig\WordPress\Model\App;
 use \Magento\Framework\Registry;
 use \Magento\Framework\Event\ObserverInterface;
 use \Magento\Store\Model\StoreManagerInterface;
+use \Magento\Framework\App\Filesystem\DirectoryList;
 
 class InjectContent implements ObserverInterface
 {
 	/**
 	  * @return
 	 **/
-	public function __construct(App $app, StoreManagerInterface $storeManager)
+	public function __construct(App $app, StoreManagerInterface $storeManager, DirectoryList $directoryList)
 	{
 		$this->_app = $app->init();
 		$this->_storeManager = $storeManager;
+		$this->directoryList = $directoryList;
 	}
 	
 	/**
@@ -31,7 +33,7 @@ class InjectContent implements ObserverInterface
 		if (!$this->_app->canRun()) {
 			return $this;
 		}
-		
+
 		if ($this->isApiRequest()) {
 			return $this;
 		}
@@ -46,13 +48,62 @@ class InjectContent implements ObserverInterface
 			$baseUrl = $this->_app->getWpUrlBuilder()->getSiteurl();
 			$jsTemplate = '<script type="text/javascript" src="%s"></script>';
 	
-			array_unshift($content, sprintf($jsTemplate, $baseUrl . 'wp-includes/js/underscore.min.js?ver=1.8.3'));
-			array_unshift($content, sprintf($jsTemplate, $baseUrl . 'wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'));
-			array_unshift($content, sprintf($jsTemplate, $baseUrl . 'wp-includes/js/jquery/jquery.js?ver=1.12.4'));
-	
-			$observer->getEvent()
-					->getResponse()
-						->setBody(str_replace('</body>', implode('', $content) . '</body>', $bodyHtml));
+			$content = implode("\n", $content);
+			
+			$scripts = array(
+				'<script type="text/javascript" src="' . $this->_app->getWpUrlBuilder()->getSiteUrl() . '/wp-includes/js/underscore.min.js"></script>',
+			);
+			
+			$scriptRegex = '<script.*<\/script>';
+			$regexes = array(
+				'<!--\[[a-zA-Z0-9 ]{1,}\]>[\s]{0,}' . $scriptRegex . '[\s]{0,}<!\[endif\]-->',
+				$scriptRegex
+			);
+		
+			// Extract all JS from $content
+			foreach($regexes as $regex) {
+				if (preg_match_all('/' . $regex . '/sUi', $content, $matches)) {
+					foreach($matches[0] as $v) {
+						$content = str_replace($v, '', $content);
+						$scripts[] = $v;
+					}
+				}
+			}
+			
+			// Check each JS file for define.amd and disable define before including then reenable after
+			foreach($scripts as $skey => $script) {
+				if (preg_match('/<script[^>]{1,}src=[\'"]{1}(.*)[\'"]{1}/U', $script, $matches)) {
+					$externalScriptUrlFull = $matches[1];
+					$externalScriptUrl = strpos($externalScriptUrlFull, '?') === false ? $externalScriptUrlFull : substr($externalScriptUrlFull, 0, strpos($externalScriptUrlFull, '?'));
+
+					// Check that the script is a local file
+					if (strpos($externalScriptUrl, $this->_app->getWpUrlBuilder()->getSiteUrl()) !== false) {
+						$localScriptFile = $this->_app->getPath() . '/' . substr($externalScriptUrl, strlen($this->_app->getWpUrlBuilder()->getSiteUrl()));
+						$scriptContent = file_get_contents($localScriptFile);
+
+						// Check whether the script supports AMD
+						if (strpos($scriptContent, 'define.amd') !== false) {
+							$newScriptFile = $this->directoryList->getPath('media') . DIRECTORY_SEPARATOR . md5($externalScriptUrlFull) . '.js';
+							$newScriptUrl = $this ->_storeManager-> getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . basename($newScriptFile);
+							
+							// Only write data if new script doesn't exist or local file has been updated
+							if (!is_file($newScriptFile) || filemtime($localScriptFile) > filemtime($newScriptFile)) {
+								file_put_contents(
+									$newScriptFile, 
+									"__d=define;define=null;\n" . $scriptContent . "\ndefine=__d;__d=null;"
+								);
+							}
+							
+							// Update script tag to use new URL					
+							$scripts[$skey] = str_replace($externalScriptUrlFull, $newScriptUrl, $script);
+						}
+					}
+				}
+			}
+
+			$content .= implode("\n", $scripts);
+
+			$observer->getEvent()->getResponse()->setBody(str_replace('</body>', $content . '</body>', $bodyHtml));
 		}
 		
 		return $this;
