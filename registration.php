ecb164988d34b1fdfeac8240921fba01d9271b1a
Ben Tideswell <ben@fishpig.co.uk>
2017-03-11 07:58:11 +0000

    1.0.0.47

--- registration.php
+++ registration.php
@@ -1,32 +1,31 @@
 <?php
+/**
+ * FishPig_WordPress by Ben Tideswell
+ * https://fishpig.co.uk/
+ */
 	
 	use \Magento\Framework\Component\ComponentRegistrar;
 
-	$vendor = 'FishPig';
-	$module = 'WordPress';
-	
 	ComponentRegistrar::register(
 		ComponentRegistrar::MODULE,
-		$vendor . '_' . $module,
+		'FishPig_WordPress',
 		__DIR__
 	);
 	
 	/*
 	 * Protection against installing module in incorrect directory
-	 * Too many emails with module installed in app/code/FishPig/Wordpress (p should be P)
 	**/
 	$currentLocation = __DIR__;
-	
+
 	if (strpos($currentLocation, 'app' . DIRECTORY_SEPARATOR . 'code' . DIRECTORY_SEPARATOR) !== false) {
-		$correctLocation = dirname(dirname($currentLocation)) . DIRECTORY_SEPARATOR . $vendor . DIRECTORY_SEPARATOR . $module;
-		$relativeLocation = $vendor . DIRECTORY_SEPARATOR . $module;
-	
-		throw new \Exception(sprintf(
-			"%s_%s is installed in the wrong folder. The module is currently in the folder %s but should be in %s. Please ensure the correct capitalisation of the module name (%s).",
-			$vendor,
-			$module,
-			$currentLocation,
-			$correctLocation,
-			$relativeLocation
-		));	
+		$relativeLocation = 'app' . DIRECTORY_SEPARATOR . 'code' . DIRECTORY_SEPARATOR . 'FishPig' . DIRECTORY_SEPARATOR . 'WordPress';
+		
+		if (strpos($currentLocation, $relativeLocation) === false) {
+			throw new \Exception(sprintf(
+				"%s is installed in the wrong folder. Please install the module in %s and make sure that you use the correct capitalisation of the module name (%s).",
+				"FishPig_WordPress",
+				$relativeLocation,
+				'FishPig' . DIRECTORY_SEPARATOR . 'WordPress'
+			));
+		}
 	}
