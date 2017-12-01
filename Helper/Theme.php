eb9f7ba6a0d6e5fa9817cb2e1fb9c2b9a0e7b6cf
Ben Tideswell <ben@fishpig.co.uk>
2017-12-01 09:03:48 +0000

    1.2.0.25

--- Helper/Theme.php
+++ Helper/Theme.php
@@ -12,48 +12,54 @@ use Magento\Framework\App\Helper\Context;
 use FishPig\WordPress\Model\Config;
 use FishPig\WordPress\Model\App\Integration\Exception as IntegrationException;
 use Magento\Framework\Module\Dir\Reader as ModuleDirReader;
+use Magento\Framework\App\State;
 
 class Theme extends \Magento\Framework\App\Helper\AbstractHelper
 {
-	/**
-	 * @var
-	**/
+	/*
+	 * @const
+	 */
 	const THEME_NAME = 'fishpig';
 	
-	/**
-	 * @var
-	**/
+	/*
+	 * @const
+	 */
 	const DS = DIRECTORY_SEPARATOR;
 	
-	/**
+	/*
 	 * @var
-	**/
+	 */
 	protected $_path = '';
 	
-	/**
+	/*
 	 * @var
-	**/
-	protected $_config = null;
+	 */
+	protected $config = null;
 	
-	/**
+	/*
 	 * @var
-	**/
+	 */
 	protected $_autoInstall = true;
 	
-	/**
-	 * @
-	**/
-    public function __construct(Context $context, Config $config, ModuleDirReader $moduleDirReader)
-    {
+	/*
+	 *
+	 *
+	 *
+	 */
+  public function __construct(Context $context, Config $config, ModuleDirReader $moduleDirReader, State $state)
+  {
 		parent::__construct($context);
-		
-	    $this->_config = $config;
-	    $this->_moduleDirReader = $moduleDirReader;
-    }
+	
+    $this->config = $config;
+    $this->moduleDirReader = $moduleDirReader;
+    $this->state = $state;
+  }
 
-	/**
-	 * @
-	**/
+	/*
+	 *
+	 *
+	 *
+	 */
 	public function setPath($path)
 	{
 		$this->_path = $path;
@@ -61,11 +67,17 @@ class Theme extends \Magento\Framework\App\Helper\AbstractHelper
 		return $this;
 	}
 
-	/**
-	 * @
-	**/
+	/*
+	 *
+	 *
+	 *
+	 */
 	public function validate()
 	{
+		if ($this->state->getAreaCode() !== 'adminhtml') {
+			return $this;
+		}
+		
 		$ds = DIRECTORY_SEPARATOR;
 		
 		if (!$this->_path || !is_dir($this->_path)) {
@@ -92,7 +104,7 @@ class Theme extends \Magento\Framework\App\Helper\AbstractHelper
 			}
 		}
 		
-		$sourceDir = $this->_moduleDirReader->getModuleDir('', 'FishPig_WordPress') . $ds . 'wptheme';
+		$sourceDir = $this->moduleDirReader->getModuleDir('', 'FishPig_WordPress') . $ds . 'wptheme';
 		$sourceFiles = scandir($sourceDir);
 		
 		foreach($sourceFiles as $sourceFile) {
@@ -126,42 +138,52 @@ class Theme extends \Magento\Framework\App\Helper\AbstractHelper
 		return $this;
 	}
 	
-	/**
-	 * @
-	**/
+	/*
+	 *
+	 *
+	 *
+	 */
 	public function isFileWriteable($file)
 	{
 		return is_file($file) && is_writeable($file) || !is_file($file) && is_writable(dirname($file));
 	}
 	
-	/**
-	 * @
-	**/
+	/*
+	 *
+	 *
+	 *
+	 */
 	public function isActive()
 	{
-		return $this->_config->getOption('template') === self::THEME_NAME
-			&& $this->_config->getOption('stylesheet') === self::THEME_NAME;
+		return $this->config->getOption('template') === self::THEME_NAME
+			&& $this->config->getOption('stylesheet') === self::THEME_NAME;
 	}
 	
-	/**
-	 * @
-	**/
+	/*
+	 *
+	 *
+	 *
+	 */
 	public function getTargetDir()
 	{
 		return $this->_path . self::DS . 'wp-content' . self::DS . 'themes' . self::DS . self::THEME_NAME;
 	}
 	
-	/**
-	 * @
-	**/
+	/*
+	 *
+	 *
+	 *
+	 */
 	public function getSourceDir()
 	{
-		return $this->_moduleDirReader->getModuleDir('', 'FishPig_WordPress') . self::DS . 'wptheme';
+		return $this->moduleDirReader->getModuleDir('', 'FishPig_WordPress') . self::DS . 'wptheme';
 	}
 	
-	/**
-	 * @
-	**/
+	/*
+	 *
+	 *
+	 *
+	 */
 	public function canAutoInstallTheme()
 	{
 		return (int)$this->_request->getParam('install-theme') === 1 || $this->_autoInstall === true;
