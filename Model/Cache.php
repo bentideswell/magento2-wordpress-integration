b19e78b0e47fc2a817febd478ddb18967bc44d99
Ben Tideswell <ben@fishpig.co.uk>
2018-01-10 08:12:16 +0000

    1.2.2.0

---
+++ Model/Cache.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+**/
+namespace FishPig\WordPress\Model;
+
+use \Magento\Framework\Cache\Frontend\Decorator\TagScope;
+use \Magento\Framework\App\Cache\Type\FrontendPool;
+
+class Cache extends TagScope
+{
+	/*
+	 * Cache type code unique among all cache types
+	 *
+	 * @const string
+	 */
+	const TYPE_IDENTIFIER = 'fishpig_wordpress';
+
+	 /*
+	  * Cache tag used to distinguish the cache type from all other cache
+	  *
+	  * @const string
+		*/
+	const CACHE_TAG = 'FISHPIG_WP';
+
+	/*
+	 * @param \Magento\Framework\App\Cache\Type\FrontendPool $cacheFrontendPool
+	 */
+	public function __construct(FrontendPool $cacheFrontendPool)
+	{
+		parent::__construct(
+			$cacheFrontendPool->get(self::TYPE_IDENTIFIER), 
+			self::CACHE_TAG
+		);
+	}
+}
