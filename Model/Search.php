d3dc84d346848133df684d6c02a1e7e47b832156
Ben Tideswell <ben@fishpig.co.uk>
2018-01-22 12:35:43 +0000

    1.2.2.3

--- Model/Search.php
+++ Model/Search.php
@@ -12,43 +12,68 @@ use FishPig\WordPress\Api\Data\Entity\ViewableInterface;
 
 class Search extends AbstractModel implements ViewableInterface
 {
-	/**
-	 * @var string
-	**/
+	/*
+	 * @const string
+	 */
 	const ENTITY = 'wordpress_search';
 	
-	/**
-	 * @var string
-	**/
+	/*
+	 * @const string
+	 */
 	const VAR_NAME = 's';
 
-	/**
-	 *
+	/*
+	 * @const string
+	 */
+	const VAR_NAME_POST_TYPE = 'post_type';
+	
+	/*
+	 * Get the search term
 	 *
 	 * @return  string
-	**/
-    public function getSearchTerm()
-    {
+	 */
+  public function getSearchTerm()
+  {
 		return $this->_viewHelper->getRequest()->getParam(self::VAR_NAME);
-    }
+  }
 
-	/**
-	 *
+	/*
+	 * Get the name of the search
 	 *
 	 * @return  string
-	**/
+	 */
 	public function getName()
 	{
 		return 'Search results for ' . $this->getSearchTerm();
 	}
 
-	/**
+	/*
+	 * Get an array of post types
+	 *
+	 * @return array
+	 */
+	public function getPostTypes()
+	{
+		return $this->_viewHelper->getRequest()->getParam(self::VAR_NAME_POST_TYPE);	
+	}
+	
+	/*
 	 *
 	 *
 	 * @return  string
-	**/
+	 */
 	public function getUrl()
 	{
-		return $this->_wpUrlBuilder->getUrl() . 'search/' . urlencode($this->getSearchTerm()) . '/';
+		$extra = '';
+		
+		if ($postTypes = $this->getPostTypes()) {
+			foreach($postTypes as $postType) {
+				$extra .= self::VAR_NAME_POST_TYPE . '[]=' . urlencode($postType) . '&';
+			}
+			
+			$extra = '?' . rtrim($extra, '&');
+		}
+		
+		return $this->_wpUrlBuilder->getUrl() . 'search/' . urlencode($this->getSearchTerm()) . '/' . $extra;
 	}
 }
