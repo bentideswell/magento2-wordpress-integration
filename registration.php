630ab2c198070f8f16bf158e844a2494f3b045fa
Ben Tideswell <ben@fishpig.co.uk>
2018-09-13 14:08:39 +0100

    2.0.1.1

--- registration.php
+++ registration.php
@@ -22,10 +22,27 @@ $functionsFile = BP . '/app/functions.php';
 
 if (is_file($functionsFile)) {
 	@file_put_contents($functionsFile, "<?php
-/*
- * Translation function has been removed for WordPress Integration
- * This file is deprecated from Magento 2.3
+/**
+ * Create value-object \Magento\Framework\Phrase
+ * @deprecated The global function __() is now loaded via Magento Framework, the below require is only
+ *             for backwards compatibility reasons and this file will be removed in a future version
+ * @see        Magento\Framework\Phrase\__.php
+ * @SuppressWarnings(PHPMD.ShortMethodName)
+ * @return \Magento\Framework\Phrase
  */
+if (!function_exists('__')) {
+	function __()
+	{
+	    $argc = func_get_args();
+	
+	    $text = array_shift($argc);
+	    if (!empty($argc) && is_array($argc[0])) {
+	        $argc = $argc[0];
+	    }
+	
+	    return new \Magento\Framework\Phrase($text, $argc);
+	}
+}
 ");
 }
 
