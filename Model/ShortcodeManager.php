227efbd7102066f323adaae36ac44bcd3005f22f
Ben Tideswell <ben@fishpig.co.uk>
2018-09-21 09:58:49 +0100

    2.0.1.6

--- Model/ShortcodeManager.php
+++ Model/ShortcodeManager.php
@@ -6,6 +6,7 @@ namespace FishPig\WordPress\Model;
 
 /* Constructor Args */
 use FishPig\WordPress\Helper\Autop;
+use Magento\Cms\Model\Template\FilterProvider;
 
 class ShortcodeManager
 {
@@ -20,13 +21,19 @@ class ShortcodeManager
 	protected $autop;
 
 	/*
+	 * @var FilterProvider
+	 */
+	protected $filterProvider;
+	
+	/*
 	 *
 	 *
 	 *
 	 */
-	public function __construct(Autop $autop, array $shortcodes = [])	
+	public function __construct(Autop $autop, FilterProvider $filterProvider, array $shortcodes = [])	
 	{
-		$this->autop = $autop;
+		$this->autop          = $autop;
+		$this->filterProvider = $filterProvider;
 		
 		foreach($shortcodes as $alias => $shortcode) {
 			if (!method_exists($shortcode, 'isEnabled') || $shortcode->isEnabled()) {
@@ -45,6 +52,9 @@ class ShortcodeManager
 		if ($args && is_object($args)) {
 			$args = ['object' => $args];
 		}
+
+		// Apply Magento block/template filters
+		$input = $this->filterProvider->getBlockFilter()->filter($input);
 
 		if ($shortcodes = $this->getShortcodes()) {
 			foreach($shortcodes as $shortcode) {
