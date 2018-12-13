1981a852d0ae7212b0d7499e08fe956ac3b96f45
Ben Tideswell <ben@fishpig.co.uk>
2018-12-13 09:38:55 +0000

    2.0.4.1

--- registration.php
+++ registration.php
@@ -8,4 +8,20 @@ use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FishPig_WordPress', __DIR__);
 
-require __DIR__ . '/functions.php';
+if (!function_exists('__')) {
+	$bootstrap    = BP . '/app/bootstrap.php';
+	$canIncludeFpFunctions = true;
+	
+	if (strpos(file_get_contents($bootstrap), 'app/functions.php') !== false) {
+		$appFunctions = BP . '/app/functions.php';
+		$fpFunctions  = __DIR__ . '/functions.php';
+		
+		if (is_file($appFunctions)) {
+			$canIncludeFpFunctions = md5_file($appFunctions) === md5_file($fpFunctions);
+		}
+	}
+
+	if ($canIncludeFpFunctions) {
+		require __DIR__ . '/functions.php';	
+	}
+}
