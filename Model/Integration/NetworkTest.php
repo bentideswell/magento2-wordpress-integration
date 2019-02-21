d7d071fb8a9e2c3d8cd36694744db1f2a844b94d
Ben Tideswell <ben@fishpig.co.uk>
2019-02-21 09:32:07 +0000

    2.0.5.12

---
+++ Model/Integration/NetworkTest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ *
+ */
+namespace FishPig\WordPress\Model\Integration;
+
+/* Constructor Args */
+use FishPig\WordPress\Model\Network;
+use FishPig\WordPress\Model\WPConfig;
+
+/* Misc */
+use FishPig\WordPress\Model\Integration\IntegrationException;
+
+class NetworkTest
+{
+	/*
+	 * @var Network
+	 */
+	protected $network;
+	
+	/*
+	 * @var WPConfig
+	 */
+	protected $wpConfig;
+
+	/*
+	 *
+	 * @param Network $network
+	 * @param WPConfig $wpConfig
+	 */
+	public function __construct(Network $network, WPConfig $wpConfig)
+	{
+		$this->network  = $network;
+		$this->wpConfig = $wpConfig;
+	}
+	
+	/*
+	 * This test checks for the situation where Multisite is enabled in WordPress
+	 * But the FishPig_WordPress_Multisite add-on is not installed in Magento
+	 *
+	 * @return $this
+	 */
+	public function runTest()
+	{
+		if ((int)$this->wpConfig->getData('MULTISITE') === 0) {
+			// Multisite not enabled in WordPress
+			return $this;
+		}
+		
+		if ($this->network->isEnabled()) {
+			// Multisite module is installed in Magento
+			return $this;
+		}
+
+		IntegrationException::throwException(sprintf(
+			'The WordPress Network is active. You must install the FishPig_WordPress_Multisite add-on module. This can be found at %s',
+			'https://fishpig.co.uk/magento/wordpress-integration/multisite/'
+		));
+	}
+}
