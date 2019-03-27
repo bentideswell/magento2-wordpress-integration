eb85efba730d331be00dde68e43018eeac6ec183
Ben Tideswell <ben@fishpig.co.uk>
2019-03-27 22:45:29 +0000

    2.0.7.1

--- wptheme/index.php
+++ wptheme/index.php
@@ -4,64 +4,39 @@
  * Any changes you make will be overwritten
  * This file is not used to display your blog.
  */
- 
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php while ( have_posts() ) : the_post() ?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>				
-					<header class="entry-header">
-						<?php
-							if ( is_single() ) :
-								the_title( '<h1 class="entry-title">', '</h1>' );
-							else :
-								the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-							endif;
-						?>
-					</header>				
-					<div class="entry-content">
-						<?php the_content() ?>
-					</div><!-- .entry-content -->				
-					<?php
-						// Author bio.
-						if ( is_single() && get_the_author_meta( 'description' ) ) :
-							get_template_part( 'author-bio' );
-						endif;
-					?>
-				
-					<footer class="entry-footer">
-
-					</footer><!-- .entry-footer -->
-				
-				</article><!-- #post-## -->
-			<?php endwhile;
-
+?>
+<?php get_header() ?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	<?php if (have_posts()): ?>
+		<?php if (is_home() && ! is_front_page()): ?>
+			<header>
+				<h1><?php single_post_title(); ?></h1>
+			</header>
+		<?php endif; ?>
+		<?php while (have_posts()): the_post() ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>				
+				<header class="entry-header">
+					<?php if (is_single()): ?>
+						<h1 class="entry-title"><?php the_title() ?></h1>
+					<?php else: ?>
+						<h2 class="entry-title"><a href="<?php echo esc_url(get_permalink()) ?>"><?php the_title() ?></a></h2>
+					<?php endif; ?>
+				</header>				
+				<div class="entry-content"><?php the_content() ?></div>
+			</article>
+		<?php endwhile ?>
+		<?php
 			// Previous/next page navigation.
 			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-			) );
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'content', 'none' );
-
-		endif;
+				'prev_text'          => __('Previous'),
+				'next_text'          => __('Next'),
+				'before_page_number' => __('Page'),
+			));
 		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
+	<?php else: ?>
+		<?php echo get_template_part('content', 'none') ?>
+	<?php endif; ?>
+	</main>
+</div>
 <?php get_footer(); ?>
-
