324e829a9eed24e7cdd2eececf56281104aa54e2
Ben Tideswell <ben@fishpig.co.uk>
2019-04-25 09:05:07 +0100

    2.0.7.15

--- Controller/Post/View.php
+++ Controller/Post/View.php
@@ -12,15 +12,15 @@ use Magento\Framework\Controller\ResultFactory;
 
 class View extends Action
 {
-	/*
-	 * Load and return a Post model
-	 *
-	 * @return \FishPig\WordPress\Model\Post|false 
-	 */
+  /*
+   * Load and return a Post model
+   *
+   * @return \FishPig\WordPress\Model\Post|false 
+   */
   protected function _getEntity()
   {
     $post = $this->factory->create('Post')->load(
-    	(int)$this->getRequest()->getParam('id')
+      (int)$this->getRequest()->getParam('id')
     );
 
     if (!$post->getId()) {
@@ -31,56 +31,76 @@ class View extends Action
   }
 
   /*
-	 *
-	 *
-	 * @return bool
-	 */
+   *
+   *
+   * @return bool
+   */
   protected function _canPreview()
   {
     return true;
   }
 
-	/*
-	 *
-	 *
-	 * @return
-	 */
-	protected function _getForward()
-	{
-  	if ($entity = $this->_getEntity()) {
-  		if ($entity->isFrontPage()) {
-				if ((int)$this->getRequest()->getParam('is_front') === 0) {
-					return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setUrl($this->url->getHomeUrl());
-				}
-  		}
-  		
-  		if ($entity->getPostStatus() === 'private' && !$this->wpContext->getCustomerSession()->isLoggedIn()) {
-    		return $this->_getNoRouteForward();
-  		}
-		}
-		
-		return parent::_getForward();
-	}
-	
   /*
-	 *
-	 */
+   *
+   *
+   * @return
+   */
+  protected function _getForward()
+  {
+    if ($entity = $this->_getEntity()) {
+      if ($entity->isFrontPage()) {
+        if ((int)$this->getRequest()->getParam('is_front') === 0) {
+          return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setUrl($this->url->getHomeUrl());
+        }
+        else {
+          // Request is static homepage (page) with a preview set (maybe visual editor)
+          foreach(['p', 'page_id', 'preview_id'] as $paramKey) {
+            if ($previewId = (int)$this->getRequest()->getParam($paramKey)) {
+              $previewPost = $this->factory->create('Post')->load($previewId);
+              
+              if ($previewPost->getId() && !$previewPost->isPublished()) {
+                $this->getRequest()->setParam('preview_id', $previewPost->getId());
+    
+                $this->registry->unregister($previewPost::ENTITY);
+                
+                return $this->resultFactory
+                  ->create(\Magento\Framework\Controller\ResultFactory::TYPE_FORWARD)
+                    ->setModule('wordpress')
+                    ->setController('post')
+                    ->forward('preview');
+              }
+            }
+          }
+        }
+      }
+      
+      if ($entity->getPostStatus() === 'private' && !$this->wpContext->getCustomerSession()->isLoggedIn()) {
+        return $this->_getNoRouteForward();
+      }
+    }
+    
+    return parent::_getForward();
+  }
+  
+  /*
+   *
+   */
   protected function _initLayout()
   {
-	  parent::_initLayout();
-	  
-	  if ($commentId = (int)$this->getRequest()->getParam('comment-id')) {
-		  $commentStatus = (int)$this->getRequest()->getParam('comment-status');
-		  
-		  if ($commentStatus === 0) {
-				$this->messageManager->addSuccess(__('Your comment has been posted and is awaiting moderation.'));
-		  }
-		  else {
-				$this->messageManager->addSuccess(__('Your comment has been posted.'));			  
-		  }
-	  }
-		
-		return $this;
+    parent::_initLayout();
+    
+    if ($commentId = (int)$this->getRequest()->getParam('comment-id')) {
+      $commentStatus = (int)$this->getRequest()->getParam('comment-status');
+      
+      if ($commentStatus === 0) {
+        $this->messageManager->addSuccess(__('Your comment has been posted and is awaiting moderation.'));
+      }
+      else {
+        $this->messageManager->addSuccess(__('Your comment has been posted.'));       
+      }
+    }
+    
+    return $this;
   }
 
   /*
@@ -90,23 +110,23 @@ class View extends Action
    */
   protected function _getBreadcrumbs()
   {
- 		if ($this->_getEntity()->isFrontPage()) {
-	 		return [];
-	 	}
-	 	
+    if ($this->_getEntity()->isFrontPage()) {
+      return [];
+    }
+    
     $crumbs = parent::_getBreadcrumbs();
   
     // Handle post type breadcrumb
     $postType = $this->getEntityObject()->getTypeInstance();
 
     if ($crumbObjects = $postType->getBreadcrumbStructure($this->getEntityObject())) {
-	    foreach($crumbObjects as $crumbType => $crumbObject) {
-	      $crumbs[$crumbType] = [
-	        'label' => (string)__($crumbObject->getName()),
-	        'title' => (string)__($crumbObject->getName()),
-	        'link' => $crumbObject->getUrl(),
-	      ];
-	    }
+      foreach($crumbObjects as $crumbType => $crumbObject) {
+        $crumbs[$crumbType] = [
+          'label' => (string)__($crumbObject->getName()),
+          'title' => (string)__($crumbObject->getName()),
+          'link' => $crumbObject->getUrl(),
+        ];
+      }
     }
 
     $crumbs['post'] = [
@@ -118,12 +138,12 @@ class View extends Action
   }
     
   /*
-	 * @return array
-	 */
+   * @return array
+   */
   public function getLayoutHandles()
   {
     if (!($post = $this->getEntityObject())) {
-	    return [];
+      return [];
     }
 
     $postType = $post->getPostType();
@@ -138,24 +158,24 @@ class View extends Action
 
     $layoutHandles = ['wordpress_post_view_default'];
     
-		if ($post->isFrontPage()) {
-			$layoutHandles[] = 'wordpress_front_page';
-		}
-		
+    if ($post->isFrontPage()) {
+      $layoutHandles[] = 'wordpress_front_page';
+    }
+    
     $layoutHandles[] = 'wordpress_' . $postType . '_view';
     $layoutHandles[] = 'wordpress_' . $postType . '_view_' . $post->getId();
 
     if ($template) {
-    	$templateName = str_replace('.php', '', $template);
-    	
+      $templateName = str_replace('.php', '', $template);
+      
 
-    	$layoutHandles[] = 'wordpress_post_view_' . $templateName;
-    	$layoutHandles[] = 'wordpress_post_view_' . $templateName . '_' . $post->getId();
+      $layoutHandles[] = 'wordpress_post_view_' . $templateName;
+      $layoutHandles[] = 'wordpress_post_view_' . $templateName . '_' . $post->getId();
 
-    	if ($postType !== 'post') {
-	    	$layoutHandles[] = 'wordpress_' . $postType . '_view_' . $templateName;
-				$layoutHandles[] = 'wordpress_' . $postType . '_view_' . $templateName . '_' . $post->getId();
-			}
+      if ($postType !== 'post') {
+        $layoutHandles[] = 'wordpress_' . $postType . '_view_' . $templateName;
+        $layoutHandles[] = 'wordpress_' . $postType . '_view_' . $templateName . '_' . $post->getId();
+      }
     }
 
     return array_merge(parent::getLayoutHandles(), $layoutHandles);
