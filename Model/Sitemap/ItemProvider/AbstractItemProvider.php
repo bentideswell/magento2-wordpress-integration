31df3d4d6ed5ed59f1fe4b25869d91fc0d5583a3
Ben Tideswell <ben@fishpig.co.uk>
2019-07-22 14:41:35 +0100

    "2.0.7.32"

--- Model/Sitemap/ItemProvider/AbstractItemProvider.php
+++ Model/Sitemap/ItemProvider/AbstractItemProvider.php
@@ -8,9 +8,7 @@ namespace FishPig\WordPress\Model\Sitemap\ItemProvider;
 use FishPig\WordPress\Model\Factory;
 use Magento\Store\Model\App\Emulation;
 use Magento\Store\Model\StoreManagerInterface;
-
-/* Misc */
-use \Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 abstract class AbstractItemProvider/* implements ItemProviderInterface*/
 {	
@@ -24,18 +22,24 @@ abstract class AbstractItemProvider/* implements ItemProviderInterface*/
 	 */
 	protected $emulation;
 	
+	/**
+   * @var ScopeConfigInterface
+   */
+  protected $scopeConfig;
+	
 	/*
 	 *
 	 *
 	 */
-	public function __construct(Factory $factory, Emulation $emulation, StoreManagerInterface $storeManager)
+	public function __construct(Factory $factory, Emulation $emulation, StoreManagerInterface $storeManager, ScopeConfigInterface $scopeConfig)
 	{
 		$this->emulation    = $emulation;
 		$this->factory      = $factory;
 		$this->storeManager = $storeManager;
-
+    $this->scopeConfig  = $scopeConfig;
+    
 		// OM required as SitemapItemInterfaceFactory is not present in Magento 2.2 and below so constructor injection breaks compilation
-		$this->itemFactory = ObjectManager::getInstance()->create('Magento\Sitemap\Model\SitemapItemInterfaceFactory');
+		$this->itemFactory = \Magento\Framework\App\ObjectManager::getInstance()->create('Magento\Sitemap\Model\SitemapItemInterfaceFactory');
 	}
 	
 	/*
@@ -46,20 +50,34 @@ abstract class AbstractItemProvider/* implements ItemProviderInterface*/
 	final public function getItems($storeId)
 	{
 		try {
+  		$items = [];
+  		
 			$this->emulation->startEnvironmentEmulation($storeId);
-			
-			$items = $this->_getItems($storeId);
+
+      if ($this->isEnabledForStore($storeId)) {
+  			$items = $this->_getItems($storeId);
+  		}
 			
 			$this->emulation->stopEnvironmentEmulation();
 		
 			return $items;
 		}
 		catch (\Exception $e) {
+  		exit($e);
 			$this->emulation->stopEnvironmentEmulation();
 			
 			throw $e;
 		}
-		
-		return [];
+	}
+	
+	/**
+   *
+   *
+   * @param int $storeId
+   * @return bool
+   */
+	protected function isEnabledForStore($storeId)
+	{
+  	return (int)$this->scopeConfig->getValue('wordpress/xmlsitemap/enabled', \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $storeId) === 1;
 	}
 }
