f1d69438f7297cd02dca4ee1dfe8d45e98dc4892
Ben Tideswell <ben@fishpig.co.uk>
2020-04-10 17:22:25 +0100

    2.0.11.16

--- wptheme/functions.php
+++ wptheme/functions.php
@@ -40,7 +40,7 @@ class FishPig_Theme
 	   	add_action('save_post',                  array($this, 'preRenderPostContent'));
 		add_action('admin_menu',                 array($this, 'onAdminMenu'));
 		add_filter('vc_front_render_shortcodes', array($this, 'onVcFrontRenderShortcodes'), 99999);
-		
+		add_filter('wp_calculate_image_srcset',  array($this, 'onWpCalculateImageSrcset'));
 		if ($this->isMagento2()) {
 			add_action('save_post', array($this, 'invalidateMagento2FPC'));
 			
@@ -435,6 +435,14 @@ class FishPig_Theme
         }
 	}
 
+    /**
+     *
+     */
+    public function onWpCalculateImageSrcset($sources)
+    {
+        return false;
+    }
+    
 	/*
 	 *
 	 *
