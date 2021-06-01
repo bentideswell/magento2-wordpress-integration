488af2dca134b47ece665365b0ec896da0a62b1d
Ben Tideswell <ben@fishpig.co.uk>
2021-06-01 16:06:24 +0100

    2.0.20.5

--- wptheme/header.php
+++ wptheme/header.php
@@ -3,11 +3,10 @@
  *
  */
 // phpcs:ignoreFile -- this file is a WordPress theme file and will not run in Magento
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+?><!doctype html>
+<html <?php language_attributes() ?>>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 <!--WP-HEAD--><?php wp_head() ?><!--/WP-HEAD-->
 </head>
 <body <?php body_class() ?>>
-	
