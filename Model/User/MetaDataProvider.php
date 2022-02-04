00f3af65b002c5d52ee4413eeff036ee9c8016fb
Ben Tideswell <ben@fishpig.co.uk>
2022-02-04 12:27:02 +0000

    3.7.0

---
+++ Model/User/MetaDataProvider.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package FishPig_WordPress
+ * @author  Ben Tideswell (ben@fishpig.com)
+ * @url     https://fishpig.co.uk/magento/wordpress-integration/
+ */
+declare(strict_types=1);
+
+namespace FishPig\WordPress\Model\User;
+
+class MetaDataProvider extends \FishPig\WordPress\Model\MetaDataProvider
+{
+}
