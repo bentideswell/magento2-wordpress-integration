75dac83f8c4e8f119ec92ecf24c1434033dc0202
Ben Tideswell <ben@fishpig.co.uk>
2022-07-15 16:17:59 +0100

    3.15.0

--- cli_commands.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package FishPig_WordPress
- * @author  Ben Tideswell (ben@fishpig.com)
- * @url     https://fishpig.co.uk/magento/wordpress-integration/
- */
-include_once __DIR__ . '/legacy_autoload.php';
-// phpcs:ignoreFile
